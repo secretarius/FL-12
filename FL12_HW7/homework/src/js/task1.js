dc7f4dc98e03fde1482dbd4bdb46edd4f819e2a2
Alex <olexandrmai@gmail.com>
2019-12-25 15:43:13 +0200

    fixed some errors

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,8 +1,11 @@
 // Your code goes here
 const ask = prompt('Please enter your e-mail:');
-const magicFive = 5;
+const validEmail = 5;
 let passWord;
 let changePass;
+let newPass;
+let newPassTwo;
+const validNewPass = 6;
 
 
 const correctInput = {
@@ -13,9 +16,9 @@ const correctInput = {
 
 if (ask === null || typeof ask === 'undefined' || ask === '') {
     alert('Canceled.');
-} else if (ask.length < magicFive) {
+} else if (ask.length < validEmail) {
     alert('I don\'t know any emails having name length less than 5 symbols');
-} else if (correctInput['user@gmail.com'] || correctInput['admin@gmail.com']) {
+} else if (correctInput['user@gmail.com']) {
     passWord = prompt('Please enter your password');
   if (!passWord) { 
     alert('Canceled.');
@@ -23,8 +26,32 @@ if (ask === null || typeof ask === 'undefined' || ask === '') {
     alert('Wrong password')
 } else {
     changePass = confirm('Do you want to change your password?');
-    if(!confirm) {
-        alert('You have failed the change.')
-    }
+    if(changePass) {
+        passWord = prompt('Please enter the old password');
+        if (!passWord) { 
+            alert('Canceled.');
+        } else if (passWord !== correctInput['user@gmail.com']) {
+            alert('Wrong password');
+        } else {
+            newPass = prompt('Please enter the new password');
+            if (!newPass) { 
+                alert('Canceled.');
+            } else if (newPass.length < validNewPass) {
+                alert('It’s too short password. Sorry.');
+            } else {
+                newPassTwo = prompt('Please enter the new password again');
+            if(newPass === newPassTwo) {
+                alert('You have successfully changed your password.');
+               correctInput['user@gmail.com'] = newPassTwo;
+            } else {
+                alert('You wrote the wrong password.');
+            }
+            } 
+        }
+    } else {
+        alert('You have failed the change.');
+    } 
 }
+} else {
+    alert('I don’t know you');
 }
